e94ca4c9d0b72df56aed26f5ad68a4f3e411ff3e
luckyl30 <168468718+luckyl30@users.noreply.github.com>
2024-04-29 23:34:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scroll to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
